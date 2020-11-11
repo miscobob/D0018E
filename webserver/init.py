d47aacba7b74e089ea7a5c46399a5bbe06fa575f
miscobob <jonsson.r.o.michael@gmail.com>
2020-11-11 10:56:06 +0000

    simple test

--- webserver/init.py
+++ webserver/init.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask import render_template
-import MySQLdb
+import mysql.connector
 
 app = Flask(__name__)
-db = MySQLdb.connect(host ="localhost", user="python", passwd ="password", db="labdb");
+db = mysql.connector.connect(user="python", passwd ="password", host ="localhost", database="labdb");
 cur = db.cursor();
 cur.execute("select * from test");
-for(row in cur.fetchall()):
-    print row[0]
+for row in cur:
+    print (row)
 
 @app.route('/')
 def start():
@@ -16,17 +16,17 @@ def start():
 @app.route('/login', methods = ["POST","GET"])
 def login():
     if(request.method == "POST"):
-
+        return False	
 @app.route('/register', methods = ["POST","GET"])
 def reqister():
     if(request.method == "POST"):
-        request.form["username"];
+        request.form["username"]
         
         
         
 def reg_user(username, password):
-    return True;
+    return True
 
 
 def validate_user(username, password):
-    return False;
+    return False
